792f86073c73c23b198e34cddb6212ac4c101350
Ml190 <117066544+Ml190@users.noreply.github.com>
2023-05-26 18:41:17 +0200

    Add files via upload

--- query5.sql
+++ query5.sql
@@ -1,12 +1,7 @@
 set @date = '';
+set @dci = '';
 select distinct p.prenom, p.nom, dp.DCI
 from dossiers_patients dp, patient p
-where dp.DCI in (
-  select dp2.DCI
-  from dossiers_patients dp2
-  where dp2.date_vente < @date
-  and dp2.NISS_patient = dp.NISS_patient 
-  and p.NISS = dp.NISS_patient
-  group by dp2.DCI
-  having COUNT(*) > 1
-);
+where dp.NISS_patient = p.NISS 
+and date_add(dp.date_prescription, interval dp.duree_traitement day)  < @date 
+and dp.DCI = @dci;
