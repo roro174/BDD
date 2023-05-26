53185af34becfc452fe39b89c2586813da6dbb96
fatimaouc <93596605+fatimaouc@users.noreply.github.com>
2023-05-26 21:44:38 +0200

    Add files via upload

--- query1.sql
+++ query1.sql
@@ -1,5 +1,5 @@
-set @nom_dci = '';
-select nom_commercial, conditionnement 
-from medicaments 
-where DCI = @nom_dci 
+set @nom_dci = '$1';
+select nom_commercial, conditionnement
+from medicaments
+where DCI = @nom_dci
 order by nom_commercial asc, conditionnement asc;
