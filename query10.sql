e8ee38cb872f53aad3bfc100430752939a60229f
fatimaouc <93596605+fatimaouc@users.noreply.github.com>
2023-05-26 21:55:50 +0200

    Update query10.sql

--- query10.sql
+++ query10.sql
@@ -1,4 +1,4 @@
-set @date_prescription = '';
+set @date_prescription = '$1';
 select distinct medicament_nom_commercial
 from dossiers_patients
 where date_prescription <  @date_prescription and medicament_nom_commercial
