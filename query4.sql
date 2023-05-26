207e0f4654dccf01ce85713eb15f9f1c6e050d76
fatimaouc <93596605+fatimaouc@users.noreply.github.com>
2023-05-26 21:47:06 +0200

    Add files via upload

--- query4.sql
+++ query4.sql
@@ -1,5 +1,5 @@
-set @nom_commercial = '';
-set @date_vente = '';
+set @nom_commercial = '$1';
+set @date_vente = '$2';
 select pt.nom, dp.date_vente
 from dossiers_patients dp, patient pt
 where pt.NISS = dp.NISS_patient and medicament_nom_commercial = @nom_commercial and date_vente > @date_vente;
